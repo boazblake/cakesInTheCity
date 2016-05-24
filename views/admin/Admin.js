da56c96b1ada3cb402d187e52d28b95a83cc9b56
boazblake <boazblake@gmail.com>
2016-05-23 19:03:57 -0500

    sending and reciving cdn from filestack

--- views/admin/Admin.js
+++ views/admin/Admin.js
@@ -1,11 +1,45 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import NavBar from '../modules/header'
 
-const handleUpload = (e) => {
-	e.preventDefault();
-	alert(event.fpfile.url);
 
-}
+
+
+(function(a){
+	if(window.filepicker){return}
+	var b =a.createElement("script");b.type="text/javascript";
+	b.async=!0;
+	b.src=("https:"===a.location.protocol?"https:":"http:")+"//api.filestackapi.com/filestack.js";
+	var c = a.getElementsByTagName("script")[0];
+		c.parentNode.insertBefore(b,c);
+	var d={};
+		d._queue=[];
+	var e="pick,pickMultiple,pickAndStore,read,write,writeUrl,export,convert,store,storeUrl,remove,stat,setKey,constructWidget,makeDropPane".split(",");
+	var f=function(a,b){
+		return function(){
+			b.push([a,arguments])
+		}
+	};
+	for(var g=0;g<e.length;g++)
+		{d[e[g]]=f(e[g],d._queue)}
+	window.filepicker=d
+})
+(document);
+
+filepicker.pick(
+ {
+    mimetype: 'image/*',
+    container: 'modal',
+    services: ['COMPUTER', 'FACEBOOK', 'CLOUDAPP']
+  },
+  function(Blob){
+    console.log(Blob.url);
+    ReactDOM.render(<img src={Blob.url}/>, document.querySelector('#a'))
+  },
+  function(FPError){
+ console.log(FPError.toString());
+  }
+);
 
 
 class Admin extends React.Component {
@@ -15,10 +49,14 @@ class Admin extends React.Component {
     	<div>
     		<h1>Admin Site</h1>
     		<NavBar />
-    		<input type="filepicker"
+    		<input	type="filepicker" 
     				data-fp-apikey="ACOZV2bQTS2gGrnnts7IRz"
-    				onchange={handleUpload}
-    				data-fp-button-class="btn btn-primary" />
+    				data-fp-mimetypes="*/*"
+    				data-fp-container="modal"
+    				data-fp-multiple="true"
+    				data-fp-services="COMPUTER,DROPBOX,FACEBOOK"
+    				onchange={filepicker}/>
+    				<div id='a'></div>
       	</div>
     )
   }
